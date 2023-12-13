a4334b7597a7825b2faf7f465f4737246399a873
amalv <1252707+amalv@users.noreply.github.com>
2023-12-13 22:34:10 +0100

    feat: differentiate API URL based on NODE_ENV

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,6 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const isProduction = process.env.NODE_ENV === "production";
+const API_URL = isProduction
+  ? "https://fhdqwwo1yj.execute-api.us-east-1.amazonaws.com/"
+  : "http://localhost:3000";
+
 export const client = new ApolloClient({
-  uri: "http://localhost:3000",
+  uri: API_URL,
   cache: new InMemoryCache(),
 });
