cb39f9a84e5078e5bbb0add5ddcbe3d047c48e20
amalv <1252707+amalv@users.noreply.github.com>
2023-12-17 21:56:31 +0100

    feat: add hover effect and transition to BookCard

--- src/components/BookList/components/BookCard/BookCard.styles.ts
+++ src/components/BookList/components/BookCard/BookCard.styles.ts
@@ -2,12 +2,32 @@ import styled from "@emotion/styled";
 import { Card, CardActionArea, CardMedia, CardMediaProps } from "@mui/material";
 
 export const CardWrapper = styled(Card)`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   margin-bottom: 16px;
   border: 1px solid #ccc;
   border-radius: 8px;
+  overflow: hidden; // Ensure child elements don't spill outside
+  transition: box-shadow 0.3s ease-in-out, transform 0.5s ease-in-out; // Longer, smoother transition
+  background: #f5f5f5;
+
+  &:hover {
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+    transform: translate(2px, 2px);
+  }
+
+  &:hover::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.1);
+    transition: background 0.3s ease-in-out; // Smoothly fade in and out
+  }
 `;
 
 export const Cover = styled(CardMedia)<CardMediaProps>`
