f1029ed7291cc1cb1ecae12f9649f9c8fb89db1e
amalv <1252707+amalv@users.noreply.github.com>
2023-12-18 23:39:31 +0100

    feat: add environment variable for production API URL

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,9 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const isProduction = process.env.NODE_ENV === "production";
-const API_URL = isProduction
-  ? import.meta.env.VITE_API_URL_PRODUCTION
-  : import.meta.env.VITE_API_URL_DEVELOPMENT;
+const API_URL =
+  import.meta.env.VITE_API_URL_PRODUCTION ||
+  import.meta.env.VITE_API_URL_DEVELOPMENT;
 
 export const client = new ApolloClient({
   uri: API_URL,
