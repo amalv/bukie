e1c55ba254b5f774e1c226ef9c6486503464cfb1
amalv <1252707+amalv@users.noreply.github.com>
2023-12-26 16:33:06 +0100

    feat: refactor BookList component and extract logic to useBookList hook

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -1,11 +1,5 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import {
-  ChangeEvent,
-  Dispatch,
-  useState,
-  useEffect,
-  SetStateAction,
-} from "react";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
+import { useBookList } from "./useBookList";
 import {
   Avatar,
   Box,
@@ -48,37 +42,34 @@ const SearchInput = ({ search, setSearch }: SearchInputProps) => {
 };
 
 export const BookList = () => {
-  const [search, setSearch] = useState("");
-  const [debouncedSearch, setDebouncedSearch] = useState("");
-  const { user, logout, isLoading } = useAuth0();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const {
+    search,
+    setSearch,
+    debouncedSearch,
+    user,
+    anchorEl,
+    handleClick,
+    handleClose,
+    handleLogout,
+    userState,
+  } = useBookList();
 
-  useEffect(() => {
-    if (user) {
-      console.log(user.sub); // user ID
-    }
-  }, [user]);
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setDebouncedSearch(search);
-    }, 500);
-
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [search]);
-
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleLogout = () => {
-    logout();
+  const userStates = {
+    loading: <CircularProgress />,
+    authenticated:
+      user && user.name ? (
+        <div>
+          <Avatar onClick={handleClick}>{user?.name[0]}</Avatar>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+        </div>
+      ) : null,
+    unauthenticated: <LoginButton />,
   };
 
   return (
@@ -87,22 +78,7 @@ export const BookList = () => {
         <Grid item xs={1} sm={1} md={2} />
         <Grid item xs={10} sm={10} md={8}>
           <Box display="flex" justifyContent="flex-end" mt={2}>
-            {isLoading ? (
-              <CircularProgress />
-            ) : user && user.name ? (
-              <div>
-                <Avatar onClick={handleClick}>{user.name[0]}</Avatar>
-                <Menu
-                  anchorEl={anchorEl}
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </Menu>
-              </div>
-            ) : (
-              <LoginButton />
-            )}
+            {userStates[userState]}
           </Box>
           <SearchInput search={search} setSearch={setSearch} />
           <Books search={debouncedSearch} limit={50} />
