7c1c81d6a67eaaece88f1735a7019686ab4b4adf
amalv <1252707+amalv@users.noreply.github.com>
2023-12-28 16:57:21 +0100

    refactor(useIntersectionObserver): extract logic into useObserverEffect hook

--- src/components/LibraryPage/components/Books/hooks/useIntersectionObserver.ts
+++ src/components/LibraryPage/components/Books/hooks/useIntersectionObserver.ts
@@ -2,34 +2,19 @@ import { useEffect, useCallback, useMemo, RefObject } from "react";
 
 const ROOT_MARGIN = "20px";
 
-export const useIntersectionObserver = (
+const createOptions = () => ({
+  root: null,
+  rootMargin: ROOT_MARGIN,
+  threshold: 1.0,
+});
+const useObserverEffect = (
   loader: RefObject<HTMLElement>,
-  handleFetchMore: (observer: IntersectionObserver) => void,
-  loading: boolean,
-  lastPageReachedRef: RefObject<boolean>
+  handleObserver: (
+    entries: IntersectionObserverEntry[],
+    observer: IntersectionObserver
+  ) => void,
+  options: IntersectionObserverInit
 ) => {
-  // Handle intersection events
-  const handleObserver = useCallback(
-    (entities: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-      const target = entities[0];
-      if (target.isIntersecting && !loading && !lastPageReachedRef.current) {
-        handleFetchMore(observer);
-      }
-    },
-    [loading, handleFetchMore, lastPageReachedRef]
-  );
-
-  // Intersection observer options
-  const options = useMemo(
-    () => ({
-      root: null,
-      rootMargin: ROOT_MARGIN,
-      threshold: 1.0,
-    }),
-    []
-  );
-
-  // Set up intersection observer
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       if (observer) {
@@ -49,4 +34,24 @@ export const useIntersectionObserver = (
       }
     };
   }, [handleObserver, options, loader]);
+};
+
+export const useIntersectionObserver = (
+  loader: RefObject<HTMLElement>,
+  handleFetchMore: (observer: IntersectionObserver) => void,
+  loading: boolean,
+  lastPageReachedRef: RefObject<boolean>
+) => {
+  const handleObserver = useCallback(
+    (entities: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      const target = entities[0];
+      if (target.isIntersecting && !loading && !lastPageReachedRef.current) {
+        handleFetchMore(observer);
+      }
+    },
+    [loading, handleFetchMore, lastPageReachedRef]
+  );
+  const options = useMemo(createOptions, []);
+
+  useObserverEffect(loader, handleObserver, options);
 };
