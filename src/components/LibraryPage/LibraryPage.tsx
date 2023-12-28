a037ff1471e8e3967050bc43100fe68565940471
amalv <1252707+amalv@users.noreply.github.com>
2023-12-28 18:44:27 +0100

    refactor: extract UI into LibraryGrid component in LibraryPage

--- src/components/LibraryPage/LibraryPage.tsx
+++ src/components/LibraryPage/LibraryPage.tsx
@@ -4,6 +4,33 @@ import { Root } from "./LibraryPage.styles";
 import { Books, Search, ThemeSwitch, UserAuthentication } from "./components";
 import { ErrorSnackbar } from "../shared";
 
+interface LibraryGridProps {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+  debouncedSearch: string;
+}
+
+const LibraryGrid: React.FC<LibraryGridProps> = ({
+  search,
+  setSearch,
+  debouncedSearch,
+}) => (
+  <Grid container>
+    <Grid item xs={0.5} sm={1} md={2} lg={2} />
+    <Grid item xs={11} sm={10} md={8} lg={8}>
+      <Box display="flex" flexDirection="column" alignItems="flex-end" mt={2}>
+        <ThemeSwitch />
+        <Box mt={2}>
+          <UserAuthentication />
+        </Box>
+      </Box>
+      <Search search={search} setSearch={setSearch} />
+      <Books search={debouncedSearch} limit={50} />
+    </Grid>
+    <Grid item xs={0.5} sm={1} md={2} lg={2} />
+  </Grid>
+);
+
 export const LibraryPage = () => {
   const { search, setSearch, debouncedSearch, error, setError } =
     useLibraryPage();
@@ -15,25 +42,11 @@ export const LibraryPage = () => {
         handleClose={() => setError(null)}
         errorMessage={error?.message ?? ""}
       />
-      <Grid container>
-        <Grid item xs={0.5} sm={1} md={2} lg={2} />
-        <Grid item xs={11} sm={10} md={8} lg={8}>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="flex-end"
-            mt={2}
-          >
-            <ThemeSwitch />
-            <Box mt={2}>
-              <UserAuthentication />
-            </Box>
-          </Box>
-          <Search search={search} setSearch={setSearch} />
-          <Books search={debouncedSearch} limit={50} />
-        </Grid>
-        <Grid item xs={0.5} sm={1} md={2} lg={2} />
-      </Grid>
+      <LibraryGrid
+        search={search}
+        setSearch={setSearch}
+        debouncedSearch={debouncedSearch}
+      />
     </Root>
   );
 };
