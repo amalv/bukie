216f12919966fe5951f82716c84f3586baf17308
amalv <1252707+amalv@users.noreply.github.com>
2024-01-09 05:03:04 +0100

    fix: update audience for Auth0Provider

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
       clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
       authorizationParams={{
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+        audience: "bukie",
         scope: "openid profile email",
         redirect_uri: redirectUri,
       }}
