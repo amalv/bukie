5577b2bb21014e74f847ccd4d275ae70549d24bf
amalv <1252707+amalv@users.noreply.github.com>
2024-01-14 23:06:18 +0100

    fix(tests): move auth.setup.ts to tests directory

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,20 +6,23 @@ dotenv.config();
 const authFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
-  const username = process.env.TEST_USERNAME;
-  const password = process.env.TEST_PASSWORD;
+  const username = process.env.VITE_AUTH0_TEST_USERNAME;
+  const password = process.env.VITE_AUTH0_TEST_PASSWORD;
 
   await page.goto("/");
   await page.getByRole("button", { name: "Log In" }).click();
+  await page.screenshot({ path: "screenshot.png" });
   await page.getByLabel("Email address").click();
   await page.getByLabel("Email address").fill(username);
   await page.getByLabel("Password").click();
   await page.getByLabel("Password").fill(password);
   await page.getByRole("button", { name: "Continue", exact: true }).click();
+
   // Wait until the page receives the cookies.
   //
   // Sometimes login flow sets cookies in the process of several redirects.
   // Wait for the final URL to ensure that the cookies are actually set.
   await page.waitForNavigation();
+
   await page.context().storageState({ path: authFile });
 });
