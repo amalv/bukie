9df3c9e06e0580c1f4d25de1d674dc660ef64e2a
amalv <1252707+amalv@users.noreply.github.com>
2024-01-22 21:46:09 +0100

    refactor: remove unused styles and update BookCard component

--- src/components/LibraryPage/components/Books/components/BookCard/BookCard.tsx
+++ src/components/LibraryPage/components/Books/components/BookCard/BookCard.tsx
@@ -1,14 +1,10 @@
 import { Box, Grid } from "@mui/material";
 
-import { Book } from "../../../../../../data/books";
-
 import { FavoriteButton, CardContent } from "./components";
 
-interface BookCardProps {
-  book: Book;
-}
+import { Book } from "@/data/books";
 
-export const BookCard: React.FC<BookCardProps> = ({ book }) => (
+export const BookCard: React.FC<{ book: Book }> = ({ book }) => (
   <Grid item xs={6} sm={4} md={4} lg={3} xl={2} key={book.title}>
     <Box position="relative">
       <FavoriteButton bookId={book.id} isFavorited={book.isFavorited} />
